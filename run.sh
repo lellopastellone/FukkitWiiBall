cc06d70c05cb5de3a03a7f1e19f8704bf2c0fb96
Leonardo Sigalini <lello@cyberDeck.lello.rocks>
2026-04-21 02:53:12 +0200

    Fix: removed useless library on the demo and added sudo command on 'start' section in run.sh file

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ "$1" == "start" ]; then
     gcc src/CarControl.c -o bin/CarControl -lwiiuse -lpigpio -lm -lbluetooth -lrt -lpthread
-    bin/CarControl
+    sudo bin/CarControl
 elif [ "$1" == "demo" ]; then
-    gcc src/CarControlDemo.c -o bin/CarControlDemo -lwiiuse -lpigpio -lm -lbluetooth -lrt -lpthread
+    gcc src/CarControlDemo.c -o bin/CarControlDemo -lwiiuse -lm -lbluetooth -lrt
     bin/CarControlDemo
 else
     echo "Usage: $0 {start|demo}"
--- src/CarControlDemo.c
+++ src/CarControlDemo.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <pigpio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <math.h>
